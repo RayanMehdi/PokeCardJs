3070a743426549c496efae4c5151c58ba33bf35d
RayanMehdi <rayanmehdi1@gmail.com>
2018-04-03 21:51:07 +0200

    Login + Signup and add pokemon to new user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -14,20 +14,30 @@ exports.login = function (req, res) {
     var tokenFB = req.body.tokenFB;
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
+    console.log(tokenFB);
     connection.query('SELECT * FROM User WHERE tokenFB = "' + tokenFB + '"', function (error, results, fields) {
         if (results.length > 0) {
             res.json({response: true});
         }
         else {
             connection.query('INSERT INTO User VALUES ("' + tokenFB + '", "' + firstName + '", "' + lastName + '")', function (error, results, fields) {
-                if(error)throw error;
                 if (error) {
                     res.json({response: false});
                 }
                 else {
                     var data = "";
-                    var requestInsert = 'INSERT INTO UsersPokemon VALUES ';
-
+                    ;
+                    var options = {
+                        port: 3000,
+                        hostname: '127.0.0.1',
+                        method: 'GET',
+                        path: '/list',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    };
+                    var response = [];
+                    var requestInsert='';
                     var request = http.get(options, (result) => {
                         result.on('data', (d) => {
                         data += d;
@@ -39,17 +49,16 @@ exports.login = function (req, res) {
                             var min = Math.ceil(1);
                             var max = Math.floor(950);
                             var pokemonId = Math.floor(Math.random() * (max - min + 1)) + min;
-                            requestInsert += '(NULL, "' + tokenFB + '", "' + pokemonId + '"),';
-                            response.push(tmpData[pokemonId]);
+                            requestInsert = 'INSERT INTO UsersPokemon(pokemonID, tokenFB) VALUES (' + pokemonId + ', "' + tokenFB + '")';
+                            response.push(tmp[pokemonId]);
+                            connection.query(requestInsert, function (error, results, fields) {
+                                if (error) {
+                                    console.error(error);
+                                    res.json({response: false});
+                                }
+                            });
                         }
-                        req = requestInsert.substring(0, requestInsert.length - 1);
-                        connection.query(requestInsert, function (error, results, fields) {
-                            if (error) {
-                                res.json({response: false});
-                            } else {
-                                res.json({response: true});
-                            }
-                        });
+
                     });
                     request.on('error', (e) => {
                         console.error(e);
@@ -64,4 +73,5 @@ exports.login = function (req, res) {
         }
         ;
     });
+    res.json({response: true});
 }
