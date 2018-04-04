6bcdf24808209b4b5d32836dfadc79dc27e7ba9e
RayanMehdi <rayanmehdi1@gmail.com>
2018-04-04 16:44:32 +0200

    Change Database file

--- BDD/PokeCard_2018-04-04.sql
+++ BDD/PokeCard_2018-04-04.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Hôte: localhost (MySQL 5.6.38)
+# Hôte: 127.0.0.1 (MySQL 5.5.5-10.1.26-MariaDB)
 # Base de données: PokeCard
-# Temps de génération: 2018-04-02 21:31:17 +0000
+# Temps de génération: 2018-04-04 14:31:51 +0000
 # ************************************************************
 
 
@@ -35,15 +35,6 @@ CREATE TABLE `Exchanges` (
   CONSTRAINT `exchanges_ibfk_1` FOREIGN KEY (`userSrc`) REFERENCES `User` (`tokenFB`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Exchanges` WRITE;
-/*!40000 ALTER TABLE `Exchanges` DISABLE KEYS */;
-
-INSERT INTO `Exchanges` (`id`, `userSrc`, `idPokemonSrc`, `idPokemonDest`)
-VALUES
-	(1,'12EFDSQF',2,9);
-
-/*!40000 ALTER TABLE `Exchanges` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Affichage de la table User
@@ -63,7 +54,10 @@ LOCK TABLES `User` WRITE;
 
 INSERT INTO `User` (`tokenFB`, `firstName`, `lastName`)
 VALUES
-	('12EFDSQF','RAYAN','MEHDI');
+	('10215483476469823','Rayan Mehdi','Rayan Mehdi'),
+	('1503212166473385','Alexandre Kessler','Alexandre Kessler'),
+	('testToken','Rayan','MEHDI'),
+	('testTooken','Rayyan','MEHDI');
 
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -77,21 +71,41 @@ DROP TABLE IF EXISTS `UsersPokemon`;
 CREATE TABLE `UsersPokemon` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `pokemonId` int(11) DEFAULT NULL,
-  `userToken` varchar(255) DEFAULT NULL,
+  `tokenFB` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `userToken` (`userToken`),
-  CONSTRAINT `userspokemon_ibfk_1` FOREIGN KEY (`userToken`) REFERENCES `User` (`tokenFB`)
+  KEY `userToken` (`tokenFB`),
+  CONSTRAINT `userspokemon_ibfk_1` FOREIGN KEY (`tokenFB`) REFERENCES `User` (`tokenFB`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `UsersPokemon` WRITE;
 /*!40000 ALTER TABLE `UsersPokemon` DISABLE KEYS */;
 
-INSERT INTO `UsersPokemon` (`id`, `pokemonId`, `userToken`)
+INSERT INTO `UsersPokemon` (`id`, `pokemonId`, `tokenFB`)
 VALUES
-	(1,2,'12EFDSQF'),
-	(2,57,'12EFDSQF'),
-	(3,463,'12EFDSQF'),
-	(5,300,'12EFDSQF');
+	(6,233,'testToken'),
+	(7,873,'testToken'),
+	(8,387,'testToken'),
+	(9,825,'testToken'),
+	(10,545,'testToken'),
+	(11,539,'testToken'),
+	(12,805,'testTooken'),
+	(13,340,'testTooken'),
+	(14,252,'testTooken'),
+	(15,315,'testTooken'),
+	(16,266,'testTooken'),
+	(17,915,'testTooken'),
+	(30,665,'1503212166473385'),
+	(31,502,'1503212166473385'),
+	(32,332,'1503212166473385'),
+	(33,312,'1503212166473385'),
+	(34,683,'1503212166473385'),
+	(35,635,'1503212166473385'),
+	(36,532,'10215483476469823'),
+	(37,349,'10215483476469823'),
+	(38,305,'10215483476469823'),
+	(39,947,'10215483476469823'),
+	(40,810,'10215483476469823'),
+	(41,842,'10215483476469823');
 
 /*!40000 ALTER TABLE `UsersPokemon` ENABLE KEYS */;
 UNLOCK TABLES;
